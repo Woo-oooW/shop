7653fa4a6aa70b6baa9f92ccaf85f790ae8005a1
Woo <swy831@gmail.com>
2024-04-28 17:50:38 +0900

    20240428

--- src/main/java/com/apple/shop/ItemController.java
+++ src/main/java/com/apple/shop/ItemController.java
@@ -1,14 +1,47 @@
 package com.apple.shop;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import javax.print.attribute.Attribute;
+import java.util.*;
+
 
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
+
+    private final ItemRepository itemRepository;
+
     @GetMapping("/list")
     String list(Model model){
-        model.addAttribute("name", "홍길동");
+        List<Item> result = itemRepository.findAll();
+        model.addAttribute("items", result);
         return "list.html";
+    }
+
+    @GetMapping("/write")
+    String write(){
+        return "write.html";
+    }
+
+    @PostMapping("/add")
+    String addPost(@ModelAttribute Item item){
+        itemRepository.save(item);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/detail/{id}")
+    String detail(@PathVariable Long id, Model model){
+        Optional<Item> result = itemRepository.findById(id);
+        if (result.isPresent()){
+            model.addAttribute("data",result.get());
+            System.out.println(result.get());
+        }
+        return "detail.html";
     }
 }
