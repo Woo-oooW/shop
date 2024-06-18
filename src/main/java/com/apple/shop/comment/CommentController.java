ceee8ca426602946ce0ca311b8dcd66e97a8f73e
Woo <swy831@gmail.com>
2024-06-18 09:55:49 +0900

    20240616-end

--- src/main/java/com/apple/shop/comment/CommentController.java
+++ src/main/java/com/apple/shop/comment/CommentController.java
@@ -1,19 +1,19 @@
-package com.apple.shop.comment;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.PostMapping;
-
-@Configuration
-@RequiredArgsConstructor
-public class CommentController {
-    private final CommentRepository commentRepository;
-
-    @PostMapping("/comment")
-    String postComment(){
-        var data = new Comment;
-
-        commentRepository.save();
-        return "";
-    }
-}
+//package com.apple.shop.comment;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.bind.annotation.PostMapping;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//public class CommentController {
+//    private final CommentRepository commentRepository;
+//
+//    @PostMapping("/comment")
+//    String postComment(){
+//        var data = new Comment;
+//
+//        commentRepository.save();
+//        return "";
+//    }
+//}
